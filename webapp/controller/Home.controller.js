6257caef1893fcd176d72dd9fc7e7b9fb395e756
ekouemensah <loyimensah@yahoo.it>
2023-10-12 09:20:31 +0200

    home principale

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -9,7 +9,15 @@ sap.ui.define([
 
         return Controller.extend("tripmanagement.tripmanagement.controller.Home", {
             onInit: function () {
+                // Get the current time
+                var oCurrentDate = new Date();
+                var sCurrentTime = oCurrentDate.getHours() + ":" + oCurrentDate.getMinutes();
 
+                // Get the TimePicker control by its ID
+                var oTimePicker = this.getView().byId("timePicker");
+
+                // Set the TimePicker value with the current time
+                oTimePicker.setValue(sCurrentTime);
             }
         });
     });
